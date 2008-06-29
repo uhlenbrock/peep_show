88574d485d4496c7e6608c5d07eaef039bc93bb1
Bobby Uhlenbrock <bobby@uhlenbrock.us>
2008-06-29 17:46:25 -0400

    Added some explanation

--- lib/peep_show.rb
+++ lib/peep_show.rb
@@ -2,6 +2,13 @@ module ActionController
   
   class Base
     
+    # Example:
+    #
+    #   # Controller
+    #   def show
+    #     @post = peep_show(:post)
+    #   end
+    #
     def peep_show(object, options = {})
       @item = object.to_s.camelize.constantize.find(params[:id])
       params[object].each_pair { |key, value| @item[key] = value } unless params[object].nil?
--- lib/peep_show_macros_helper.rb
+++ lib/peep_show_macros_helper.rb
@@ -1,5 +1,18 @@
 module PeepShowMacrosHelper
   
+  # Makes an HTML link which will (optionally) pop a new window and render 
+  # the dirty object for review.
+  #
+  # On click, the current form data will be passed to the show action of
+  # the current controller and merged with associations to render a preview.
+  # 
+  # <tt>name</tt>::               The link text (eg, Preview)
+  # <tt>object</tt>::             A reference to the current content (eg, f.object or @post)
+  #
+  # Addtional +options+ are:
+  # <tt>:form_id</tt>::           For use when the rails standard form id (eg, edit_post_1) is not in use
+  # <tt>:url</tt>::               An alternate URL to render the dirty preview (default is /posts/show/1)
+  # <tt>:popup</tt>::             Setting to false will show the preview in the same window
   def submit_to_peep_show(name, object, options = {})
     options[:form_id] ||= dom_id(object, :edit)
     options[:url] ||= %{/#{params[:controller]}/show/#{object.id}}
@@ -8,7 +21,6 @@ module PeepShowMacrosHelper
     function << %{$("#{options[:form_id]}").writeAttribute("target","_blank");} if options[:popup]
     function << %{$("#{options[:form_id]}").writeAttribute("action","#{options[:url]}");}
     function << %{$("#{options[:form_id]}").submit();}
-    function << %{alert("enjoy the preview");}
     function << %{$("#{options[:form_id]}").writeAttribute("target","_self");} if options[:popup]
     function << %{$("#{options[:form_id]}").writeAttribute("action",action);}
     link_to_function(name, function)
